d2fc92871e1808d665b3238046259263a8bf3bf8
André Souza Nunes <Andr Souza Nunes>
2019-06-25 01:15:44 -0400

    melhoria nos formularios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form, Input } from '@rocketseat/unform';
+import { Form, Input, FileInput } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
 const schema = Yup.object().shape({
@@ -14,15 +14,24 @@ const schema = Yup.object().shape({
       .required(),
     otherwise: Yup.string().strip(true),
   }),
+  attach: Yup.string()
 });
 
 function App() {
   const [updatePassword, setUpdatePassword] = useState(false);
+  const [updateAttach, setUpdateAttach] = useState('');
+  
 
   const initialData = {
     name: 'John Doe',
     email: 'johndoe@example.com',
+    attach: 'sdsd'
   };
+
+  function handleProgress(progress, event) {
+    console.log('Evento:',event.target.result)
+    setUpdateAttach(event.target.result)
+  }
 
   function handleSubmit(data) {
     console.log('Data:', data)
@@ -32,22 +41,29 @@ function App() {
     <Form
       schema={schema}
       initialData={initialData}
-      context={{ updatePassword }}
+      context={{ updatePassword, updateAttach }}
       onSubmit={handleSubmit}
     >
-      <Input name="name" />
-      <Input name="email" />
+      <Input name="name" /><br/>
+      <Input name="email"/><br/>
 
       <input
         type="checkbox"
         name="Update Password"
         checked={updatePassword}
         onChange={e => setUpdatePassword(e.target.checked)}
-      />
+      /><br/>
 
-      <Input name="password" type="password" />
+      <Input name="password" type="password" /><br/>
+
+      <FileInput name="attach" onStartProgress={handleProgress} /><br/>
 
       <button type="submit">Save</button>
+
+      <img src={updateAttach} 
+           alt="Imagem" 
+           height={150}
+           width={150} />
     </Form>
   );
 }
